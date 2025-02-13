d1b3bf30a0e1bc393850a6e86dd41207e3680a39
Mark Vayngrib <mvayngrib@gmail.com>
2025-02-13 11:31:52 -0500

    chore: prettier

--- src/helpers/parseUtil.ts
+++ src/helpers/parseUtil.ts
@@ -148,7 +148,7 @@ export class ParseStatus {
       if (value.status === "dirty") status.dirty();
 
       if (
-        !['__proto__', 'prototype', 'constructor'].includes(key.value) &&
+        !["__proto__", "prototype", "constructor"].includes(key.value) &&
         (typeof value.value !== "undefined" || pair.alwaysSet)
       ) {
         finalObject[key.value] = value.value;
